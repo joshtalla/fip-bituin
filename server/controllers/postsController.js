fa8de14859615fbfdaec9d380f4248a9d7ef3ad8
ck <chkendal@uci.edu>
2026-04-15 00:14:36 -0700

    finished requirements

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -2,20 +2,42 @@ const { insertPost } = require('../services/postService');
 
 const createPost = async (req, res) => {
   try {
+    const { prompt_id, content } = req.body;
+
+    
+    if (!content) {
+      return res.status(400).json({ error: "Content required" });
+    }
+
+    
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ error: "No auth token" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    
+    const { data, error } = await supabase.auth.getUser(token);
+
+    if (error || !data.user) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
+    const auth_user_id = data.user.id;
 
     const post = await insertPost({
-      prompt_id: "testing-pleasework-prompt",
-      content: "HELLO FREE NEWJEANS YESSS"
+      prompt_id,
+      content,
+      auth_user_id
     });
 
-    res.status(201).json(post);
+    return res.status(201).json(post);
 
   } catch (err) {
-    console.error("FULL ERROR:", err);
-    res.status(500).json({ error: err.message });
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
   }
 };
 
-module.exports = {
-  createPost
-};
+module.exports = { createPost };
--- server/services/postService.js
+++ server/services/postService.js
@@ -1,20 +1,30 @@
-const supabase = require('../supabaseClient');
+const { supabase } = require('../supabaseClient');
 
-const insertPost = async (post) => {
+const insertPost = async ({ prompt_id, content, auth_user_id }) => {
+
+  // get user profile
+  const { data: user } = await supabase
+    .from('users')
+    .select('anonymous_name')
+    .eq('id', auth_user_id)
+    .single();
+
+  const containsEmail = /\S+@\S+\.\S+/.test(content);
+
   const { data, error } = await supabase
     .from('posts')
-    .insert(post)
+    .insert({
+      prompt_id,
+      content,
+      anonymous_name: user.anonymous_name,
+      is_flagged: containsEmail
+    })
     .select()
     .single();
 
-  if (error){
-    console.error('Supabase insert error:')
-    throw error;
-  }
-  
+  if (error) throw error;
+
   return data;
 };
 
-module.exports = {
-  insertPost
-};
+module.exports = { insertPost };
