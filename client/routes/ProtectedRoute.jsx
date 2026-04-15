bcf79e998a8223c3c9b7fdae0fe0894f7d7da71f
Sean San <seanksan@gmail.com>
2026-04-14 18:29:08 -0700

    feat: Route user based on if they are logged in or not & loading state

--- client/routes/ProtectedRoute.jsx
+++ client/routes/ProtectedRoute.jsx
@@ -3,12 +3,24 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
+    // Get the logged-in user's profile and loading state from the AuthContext.
     const { user, loading } = useContext(AuthContext);
 
+    // If the user is still loading, return null which can be used
     if (loading) {
         return null;
     }
 
+    // If it isnt loading but there's no user, it means the user is not logged in. 
+    // Therefore redirect to the login page.
+    if (!user) {
+        return <Navigate to="/login" replace />;
+    }
+
+    // If it isn't loading and there is a user, it means the user is logged in. 
+    // Return 'children' which can be used to know that it is ok to display 
+    // the pages that a logged-in user should see.
+    return children;
 };
 
 export default ProtectedRoute;
